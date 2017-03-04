b316b8c19ce10567dd1a9b674bfc567732a04c48
kimura keisuke <kimura@n-create.co.jp>
2017-03-05 08:39:20 +0900

    Revert "kkeisuke's data"
    This reverts commit aaf4e9d9335147016e78e1adf6f1507da2b05d80.

--- scrape.py
+++ scrape.py
@@ -148,7 +148,7 @@ def get_code_page(url):
 
 
 if __name__ == '__main__':
-    code_pages = get_user('kkeisuke')
+    code_pages = get_user('Saqoosha')
     for p in code_pages:
         print(p)
         get_code_page(p)
